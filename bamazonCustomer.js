8e89c9060d964aa5655d5d231bb6adc066b88491
apizarras <angelalee.p@gmail.com>
2019-05-11 17:33:35 -0500

    update code to display all products as part of inquirer prompt and added additional prompt for quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,13 +19,54 @@ connection.connect(function(err) {
 //initiate app with list of products
 function startStore() {
     displayAllProducts();
+    // The first should ask them the ID of the product they would like to buy.
+    // The second message should ask how many units of the product they would like to buy.
+    
 };
 
 function displayAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
-        for(i=0;i<res.length; i++) {
-            console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].deparment_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-            console.log("\n-------------------------------------------\n");
-        };
+        if(err) throw err;
+        inquirer
+          .prompt([
+            {
+                name: "choice",
+                type: "rawlist",
+                message: "Please select the product you would like to purchase.",
+                choices: function() {
+                    const choiceArray = [];
+                    for(i=0;i<res.length;i++) {
+                        choiceArray.push(res[i].product_name)
+                    }
+                    return choiceArray;
+                }
+            }
+        ]).then(function(answer) {
+            let choice;
+            for(i=0;i<res.length;i++) {
+                if(res[i].product_name===answer.choice) {
+                    choice = res[i];
+                };
+            };
+            inquirer
+                .prompt ([
+                    {
+                        name: "quantity",
+                        type: "input",
+                        message: "How many would you like?",
+                    }
+                ]).then(function() {
+                    checkInventory();
+                })
+        });
     });
 };
+
+function checkInventory(quantity) {
+    //compare requested quantity to instock quantity
+    if(answer.quantity>results.stock_quantity) {
+        console.log("There isn't enough stock to meet your request. Try again.")
+    } else {
+        console.log("Your purchase total...");
+    }
+}
