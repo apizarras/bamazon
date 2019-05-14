25880c9ebca53fb8b3d12f9a1da9637d08840507
apizarras <angelalee.p@gmail.com>
2019-05-13 21:53:02 -0500

    function to check inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,7 @@ connection.connect(function(err) {
 
 //initiate app with list of products
 function startStore() {
+    // The first should ask them the ID of the product they would like to buy.
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
         inquirer
@@ -42,50 +43,16 @@ function startStore() {
                     choice = res[i];
                     console.log(res[i]);
                     console.log("this is choice: " + choice.product_name);
+                    console.log("")
                 };
             };
+            // The second message should ask how many units of the product they would like to buy.
+            checkInventory(choice);
         });
     });
-    // The first should ask them the ID of the product they would like to buy.
-    // The second message should ask how many units of the product they would like to buy.
-    
 };
 
-// function displayAllProducts() {
-//     connection.query("SELECT * FROM products", function(err, res) {
-//         if(err) throw err;
-//         inquirer
-//           .prompt([
-//             {
-//                 name: "choice",
-//                 type: "rawlist",
-//                 message: "Please select the product you would like to purchase.",
-//                 choices: function() {
-//                     const choiceArray = [];
-//                     for(i=0;i<res.length;i++) {
-//                         choiceArray.push(res[i].product_name)
-//                     }
-//                     return choiceArray;
-//                 }
-//             }
-//         ]).then(function(answer) {
-//             // let choice = "";
-//             // console.log(res);
-//             for(i=0;i<res.length;i++) {
-//                 if(res[i].product_name===answer.choice) {
-//                     var choice = res[i];
-//                     console.log(res[i]);
-//                     console.log("this is choice: " + choice);
-//                 };
-//             };
-//         });
-//     });
-// };
-
-// function askQuantity() {
-
-// }
-function checkInventory(quantity) {
+function checkInventory(choice) {
     inquirer
         .prompt ([
             {
@@ -94,16 +61,19 @@ function checkInventory(quantity) {
                 message: "How many would you like?",
             }
         ]).then(function(answer) {
+            connection.query("SELECT * FROM products WHERE product_name=?", [choice], function(err, res) {
+                console.log("this is result of 2nd connection: "+choice);
+                
+            //compare requested quantity to instock quantity
             let quantity = answer.quantity;
-            let stockOnhand = res.stock_quantity;
-            console.log("this is onhand: "+stockOnhand)
+            let stockOnhand = choice.stock_quantity;
+            console.log("this is onhand: "+stockOnhand);
+            console.log("this is user requested quantity: "+quantity);
+                if(parseInt(quantity)>stockOnhand) {
+                    console.log("There isn't enough stock to meet your request. Try again.")
+                } else {
+                    console.log("Your purchase total...");
+                }
+            });
         });
-    //compare requested quantity to instock quantity
-    if(quantity>stockOnhand) {
-        console.log("There isn't enough stock to meet your request. Try again.")
-    } else {
-        console.log("Your purchase total...");
-    }
-//exit connection
-    connection.end();
 };
